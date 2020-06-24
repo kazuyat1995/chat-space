03cd3b74b2f363ed94a6e33f60ac01daccd3694d
kazuyat1995 <kazuyat1995@gmail.com>
2020-06-24 11:00:49 +0900

    自動更新機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    # namespace :ディレクトリ名 do ~ endと囲む形でルーティングを記述すると、そのディレクトリ内のコントローラのアクションを指定できる。
+    # controllersディレクトリ直下にさらにディレクトリを作成した場合、ルーティングをそれに対応させるための書き方。 namespace :ディレクトリ名 do ~ end
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
 end
